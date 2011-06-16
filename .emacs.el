b7bff5edb9a45657fa4f7cc91a2aef603c47f255
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2011-06-16 14:04:19 +0200

    Python mod modified

--- .emacs.el
+++ .emacs.el
@@ -11,24 +11,49 @@
 (require 'ido)
   (ido-mode t)
 ;;
-;; Revive (save layouts)
+;; transform file to htmlified !
+;;
+(require 'htmlize)
+;;
+;; shell-toggle-cd
+;;
+(autoload 'shell-toggle-cd "~/.emacs.d/shell-toggle.el"
+  "Pops up a shell-buffer and insert a \"cd <file-dir>\" command." t)
+;;
+;; Python mod
+;;
+(require 'python-mode)
+(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+    (setq interpreter-mode-alist (cons '("python" . python-mode)
+                                       interpreter-mode-alist))
+    (autoload 'python-mode "python-mode" "Python editing mode." )
+;;
+;; AUTO tab
+;;
+(add-hook 'c-mode-common-hook '(lambda ()
+				 (local-set-key (kbd "RET") 'newline-and-indent)))
+;;
+;; Save layout (revive.el)
 ;;
 (autoload 'save-current-configuration "revive" "Save status" t)
 (autoload 'resume "revive" "Resume Emacs" t)
 (autoload 'wipe "revive" "Wipe Emacs" t)
 ;;
-;; Espresso
+;; Best JS mod (Nespresso)
 ;; 
 (autoload 'espresso-mode "espresso" "Start espresso-mode" t)
 (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
-;;
-;; Rinari (Rails)
-;;
+
+;;Ruby emacs : https://github.com/remvee/emacs-rails
+
+;(add-to-list 'load-path (expand-file-name "~/.emacs.d/rails-minor-mode"))
+;(require 'rails)
+
+
+ ;; Rinari
 (add-to-list 'load-path "~/.emacs.d/rinari")
 (require 'rinari)
-;; Auto indent when RET
-(add-hook 'ruby-mode-hook '(lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
 
 ;;
 ;; ERB & co
@@ -51,7 +76,6 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/sass-mode"))
 (require 'sass-mode)
 (add-to-list 'auto-mode-alist '("\\.scss$" . sass-mode))
-(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
 ;;
 ;; MARKDOWN
 ;;
@@ -64,29 +88,6 @@
 ;;
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/coffee-mode"))
 (require 'coffee-mode)
-;;
-;; Htmlized
-;;
-(require 'htmlize)
-;;
-;; shell-toggle-cd
-;;
-(autoload 'shell-toggle-cd "~/.emacs.d/shell-toggle.el"
-  "Pops up a shell-buffer and insert a \"cd <file-dir>\" command." t)
-;;
-;; Python mod
-;;
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-    (setq interpreter-mode-alist (cons '("python" . python-mode)
-                                       interpreter-mode-alist))
-    (autoload 'python-mode "python-mode" "Python editing mode." )
-
-
-;;
-;;
-;; Configuration
-;;
-;;
 
 ;;
 ;; COLOR PARENTHESIS
@@ -94,7 +95,7 @@
 (load-library "paren")
 (show-paren-mode 1)
 ;;
-;; Some confs
+;; DISABLE BACKUP FILES
 ;;
 (setq make-backup-files nil)
 (setq delete-auto-save-files t)
@@ -102,15 +103,6 @@
 (setq-default indicate-empty-lines t)
 (transient-mark-mode t)
 (column-number-mode t)
-(setq inhibit-startup-message t)
-(setq frame-title-format "%S: %f")
-(modify-frame-parameters nil '((wait-for-wm . nil)))
-(fset 'yes-or-no-p 'y-or-n-p)
-(icomplete-mode 1)
-(setq column-number-mode t)
-(setq display-time-string-forms '((format "[%s:%s]-[%s/%s/%s] " 24-hours minutes day month year)))
-(setq scroll-preserve-screen-position t)
-(add-hook 'save-buffer-hook 'delete-trailing-whitespace)
 ;;
 ;; COLORS
 ;;
@@ -120,6 +112,18 @@
 (set-face-background 'show-paren-mismatch-face "Magenta")
 (set-face-foreground 'show-paren-mismatch-face "Red")
 (set-face-foreground 'highlight "yellow")
+;;
+;; MISC
+;;
+(setq inhibit-startup-message t)
+(setq frame-title-format "%S: %f")
+(modify-frame-parameters nil '((wait-for-wm . nil)))
+(fset 'yes-or-no-p 'y-or-n-p)
+(icomplete-mode 1)
+(setq column-number-mode t)
+(setq display-time-string-forms '((format "[%s:%s]-[%s/%s/%s] " 24-hours minutes day month year)))
+(setq scroll-preserve-screen-position t)
+(add-hook 'save-buffer-hook 'delete-trailing-whitespace)
 ;; 
 ;; SHORTCUTS
 ;;
@@ -137,27 +141,4 @@
 (global-set-key (kbd "<f6>") 'enlarge-window-horizontally)
 (global-set-key (kbd "<f7>") 'shrink-window)
 (global-set-key (kbd "<f8>") 'enlarge-window)
-;;
-;; AUTO tab
-;;
-(add-hook 'c-mode-common-hook '(lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
-
-;;
-;; Remove "Active processes exists [...]" at exit
-;; http://stackoverflow.com/questions/2706527/make-emacs-stop-asking-active-processes-exist-kill-them-and-exit-anyway
-;;
-(defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
-  "Prevent annoying \"Active processes exist\" query when you quit Emacs."
-  (flet ((process-list ())) ad-do-it))
-
-;;
-;; Remove useless toolbar
-;;
-(menu-bar-mode)
-
-;;
-;; Startup autoconf
-;;
-;;(split-window-horizontally)
-;;(shell-toggle-cd)
 
